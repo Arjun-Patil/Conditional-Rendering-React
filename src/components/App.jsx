53006829872a669be323b2cb0ba51672f359a444
Arjun Patil <arjunshivaji100@gmail.com>
2021-05-11 16:38:22 +0530

    Done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,10 @@
 import React from "react";
+import Form from "./form";
+var isLoggedIn = false;
 
 function App() {
   return (
-    <div className="container">
-      <h1>Hello</h1>
-      <form className="form">
-        <input type="text" placeholder="Username" />
-        <input type="password" placeholder="Password" />
-        <button type="submit">Login</button>
-      </form>
-    </div>
+    <div className="container">{isLoggedIn ? <h1>Hello</h1> : <Form />}</div>
   );
 }
 
